401a4926b7fdecf87808e1972e50e85cda350209
ptrela <pawel.trela@siepomaga.pl>
2022-06-22 00:07:48 +0200

    [WEB-001] Added blogging bounded context

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module DddStore
   class Application < Rails::Application
+    config.paths.add 'blogging/lib', eager_load: true
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
